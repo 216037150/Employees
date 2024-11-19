b38977739d20e8aa862b778d588c9943ff9b6e12
216037150 <siyabongazungu95@gmail.com>
2024-11-20 00:45:12 +0200

    server / server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'dist', 'employees', 'browser')));
+app.use(express.static(path.join(__dirname, '..', 'dist', 'employees', 'browser')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'employees', 'browser', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist', 'employees', 'browser', 'index.html'));
 });
 
 app.listen(port, () => {
