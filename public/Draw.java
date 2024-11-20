5ff12e15160c21c8698e7204908e4990d726c0dd
216037150 <siyabongazungu95@gmail.com>
2024-11-19 23:20:11 -0500

    server.js

--- public/Draw.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-public class Draw {
-  public static void main(String[] args) {
-    var name = JOptionPane.showInputDialog("What is your name?");
-    var output = name + " is such a nice name!";
-    JOptionPane.showMessageDialog(null, output);
-  }
-}
