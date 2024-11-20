cd9e7d2fea2bc48ff0e051509d2b597951406ec0
216037150 <siyabongazungu95@gmail.com>
2024-11-20 06:52:08 +0200

    npm run start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-export { AppServerModule } from './app/app.server.module';
-export { renderModule } from '@angular/platform-server';
+// export { AppServerModule } from './app/app.server.module';
+// export { renderModule } from '@angular/platform-server';
 
 
 bootstrapApplication(AppComponent, appConfig)
